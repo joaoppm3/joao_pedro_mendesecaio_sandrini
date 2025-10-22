588cc0afc7c1d73328c8fcff32f774e7e0a24ab8
caio-sandrini <caiocs4@al.insper.edu.br>
2025-10-22 10:22:55 -0300

    teste

---
+++ funcoes.py
@@ -0,0 +1,17 @@
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    lista_posicao = []
+    if orientacao == 'horizontal':
+        i = 0
+        while i < tamanho:
+            posicao = [linha,coluna]
+            lista_posicao.append(posicao)
+            coluna += 1
+            i += 1
+    if orientacao == 'vertical':
+        j = 0
+        while j < tamanho:
+            position = [linha,coluna]
+            lista_posicao.append(position)
+            linha += 1
+            j += 1
+    return lista_posicao
