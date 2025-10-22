adaff8d28a7408423195f6ed5e4454a2d818879e
caio-sandrini <caiocs4@al.insper.edu.br>
2025-10-22 17:18:38 -0300

    Ex2

--- funcoes.py
+++ funcoes.py
@@ -15,3 +15,11 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
             linha += 1
             j += 1
     return lista_posicao
+
+def preenche_frota(frota,nome_navio,linha,coluna,orientacao,tamanho):
+    posicoes = define_posicoes(linha,coluna,orientacao,tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else: 
+        frota[nome_navio] = [posicoes]
+    return frota
